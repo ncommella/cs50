5c624b1f61bf0f68327ad4ef773c460bc3fa4e1a
Nick <ncommella@gmail.com>
2021-02-01 12:33:13 -0500

    Mario.c nearly completed with one input validation task left.

--- psets/week1/mario.c
+++ psets/week1/mario.c
@@ -1,1 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int height;
+  int spaces;
+  //TODO: validate input so letters don't crash program
+  //get height from user and verify it is valid
+  do
+  {
+    printf("Height:\n");
+    scanf("%d", &height);
+  }
+  while(height < 1 || height > 8);
+
+  //set spaces equal to one less than height
+  spaces = height - 1;
+
+  //print hashes with the appropriate amount of spaces first.
+  //row for loop
+  for (int i = 0; i < height; i++)
+  {
+    //spaces for loop
+    for(int j = 0; j < spaces; j++)
+    {
+      printf(" ");
+    }
+    //left hash for loop
+    for (int k = 0; k < height - spaces; k ++)
+    {
+      printf("#");
+    }
+
+    //print middle spaces
+    printf("  ");
+
+    //right hash for loop
+    for (int k = 0; k < height - spaces; k ++)
+    {
+      printf("#");
+    }
+
+    //move to next row and decrement spaces
+    printf("\n");
+    spaces--;
+  }
+
+  return 0;
+}
